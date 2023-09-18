bbc7eb56ecfe61ab18d08f13cda99480be6b2f21
Senthil Sivanath <senthil.ss@klnce.edu>
2023-09-19 01:21:05 +0530

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,12 +3,12 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span><img style={{display:'inline',marginRight:12}} src="https://radical.sh/radical-logo-image.png" height="42" width="42" />Radical</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
+  // project: {
+  //   link: 'https://github.com/shuding/nextra-docs-template',
+  // },
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
     text: 'Radical',
