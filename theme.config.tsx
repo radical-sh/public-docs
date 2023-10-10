d8799a0a0d07e75da6a9aba3d1d52d3656c90962
Senthil Sivanath <senthil.ss@klnce.edu>
2023-10-10 15:20:41 +0530

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -9,7 +9,7 @@ const config: DocsThemeConfig = {
   // chat: {
   //   link: 'https://discord.com',
   // },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/radical-sh/public-docs',
   footer: {
     text: 'Radical',
   },
