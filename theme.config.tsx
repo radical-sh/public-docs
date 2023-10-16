8b504bd3af44cee22f32d1db381cfc0d2d53307a
SenthilSivanath <senthil.ss@klnce.edu>
2023-10-16 11:09:50 +0530

    defaulted to light

--- theme.config.tsx
+++ theme.config.tsx
@@ -11,18 +11,16 @@ const config: DocsThemeConfig = {
     defaultMenuCollapseLevel: 1,
     toggleButton: true
   },
-  nextThemes:{
-    defaultTheme:'light'
-  },
   darkMode: false,
   search: {
     component: () => {
-      return (<ul id="header-menu" className='no-float'>
-        <li><a className='nav-link' href="https://radical.sh">Overview</a></li>
-        <li><a className='nav-link' href="https://radical.sh#why">Why?</a></li>
-        <li><a className='nav-link' href="https://radical.sh#features">Features</a></li>
-        <li><a className='nav-link active' href="#">Documentation</a></li>
-      </ul>)
+      return (
+        <ul id="header-menu" className='no-float'>
+          <li><a className='nav-link' href="https://radical.sh">Overview</a></li>
+          <li><a className='nav-link' href="https://radical.sh#why">Why?</a></li>
+          <li><a className='nav-link' href="https://radical.sh#features">Features</a></li>
+          <li><a className='nav-link active' href="#">Documentation</a></li>
+        </ul>)
     }
   },
   navbar: {
@@ -35,7 +33,7 @@ const config: DocsThemeConfig = {
 
 
       useEffect(() => {
-    
+
         const handleRouteChange = (url) => {
           const asideElement = document.getElementsByTagName("aside")
           asideElement[0].className = "nextra-sidebar-container nx-flex nx-flex-col md:nx-top-16 md:nx-shrink-0 motion-reduce:nx-transform-none nx-transform-gpu nx-transition-all nx-ease-in-out md:nx-w-64 md:nx-sticky md:nx-self-start max-md:[transform:translate3d(0,-100%,0)]"
@@ -51,55 +49,69 @@ const config: DocsThemeConfig = {
         }
       }, [router])
 
-      return <div id="header">
-        <div id="header-logo">
-          <Image
-            src={logo}
-            alt='Radical Logo'
-            className="mt-2 radical-home-logo"
-            style={{
-              width: 48,
-              height: 48,
-              display: 'inline',
+      return (
+        <>
+          <script
+            lang="javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+      window.localStorage.setItem("theme", "light");
+      window.localStorage.setItem("theme_default", "light");
+      document.documentElement.classList.add("light");
+      document.documentElement.classList.remove("light");
+    `,
+            }}
+          /> <div id="header">
+            <div id="header-logo">
+              <Image
+                src={logo}
+                alt='Radical Logo'
+                className="mt-2 radical-home-logo"
+                style={{
+                  width: 48,
+                  height: 48,
+                  display: 'inline',
 
-              background: 'linear-gradient(135deg, rgba(255, 255, 255, 0.4) 0%, rgba(255, 255, 255, 0) 100%)',
-              filter: 'brightness(1.1) saturate(0.7) hue-rotate(9deg)',
-              transition: 'transform 0.3s ease-out',
+                  background: 'linear-gradient(135deg, rgba(255, 255, 255, 0.4) 0%, rgba(255, 255, 255, 0) 100%)',
+                  filter: 'brightness(1.1) saturate(0.7) hue-rotate(9deg)',
+                  transition: 'transform 0.3s ease-out',
 
-            }}
-            onMouseEnter={(e) => {
-              e.target.style.transform = 'rotate(10deg)';
-            }}
-            onMouseLeave={(e) => {
-              e.target.style.transform = 'rotate(0deg)';
-            }}
-          />
-          <h1 className="d-inline ms-4 mt-2 fs-2 radical-home-header">Radical</h1>
-        </div>
-        <div>
-          <ul id="header-menu" className='hide-on-xs'>
-            <li><a className='nav-link' href="https://radical.sh">Overview</a></li>
-            <li><a className='nav-link' href="https://radical.sh#why">Why?</a></li>
-            <li><a className='nav-link' href="https://radical.sh#features">Features</a></li>
-            <li><a className='nav-link active' href="#">Documentation</a></li>
-            {/* <li id="search">
+                }}
+                onMouseEnter={(e) => {
+                  e.target.style.transform = 'rotate(10deg)';
+                }}
+                onMouseLeave={(e) => {
+                  e.target.style.transform = 'rotate(0deg)';
+                }}
+              />
+              <h1 className="d-inline ms-4 mt-2 fs-2 radical-home-header">Radical</h1>
+            </div>
+            <div>
+              <ul id="header-menu" className='hide-on-xs'>
+                <li><a className='nav-link' href="https://radical.sh">Overview</a></li>
+                <li><a className='nav-link' href="https://radical.sh#why">Why?</a></li>
+                <li><a className='nav-link' href="https://radical.sh#features">Features</a></li>
+                <li><a className='nav-link active' href="#">Documentation</a></li>
+                {/* <li id="search">
               {d({})}
             </li> */}
-          </ul>
-        </div>
-        <div id="login-nav-button"><a href="http://radical.sh/dashboard">Sign In</a></div>
-        <button onClick={() => {
-          const asideElement = document.getElementsByTagName("aside")
-          //asideElement[0].className = "nextra-sidebar-container nx-flex nx-flex-col md:nx-top-16 md:nx-shrink-0 motion-reduce:nx-transform-none nx-transform-gpu nx-transition-all nx-ease-in-out md:nx-w-64 md:nx-sticky md:nx-self-start max-md:[transform:translate3d(0,-100%,0)]"
-          if (!menuOpen) {
-            asideElement[0].className = "nextra-sidebar-container nx-flex nx-flex-col md:nx-top-16 md:nx-shrink-0 motion-reduce:nx-transform-none nx-transform-gpu nx-transition-all nx-ease-in-out md:nx-w-64 md:nx-sticky md:nx-self-start max-md:[transform:translate3d(0,0,0)]"
-          }
-          else {
-            asideElement[0].className = "nextra-sidebar-container nx-flex nx-flex-col md:nx-top-16 md:nx-shrink-0 motion-reduce:nx-transform-none nx-transform-gpu nx-transition-all nx-ease-in-out md:nx-w-64 md:nx-sticky md:nx-self-start max-md:[transform:translate3d(0,-100%,0)]"
-          }
-          setIsMenuOpen(!menuOpen)
-        }} type="button" aria-label="Menu" className="nextra-hamburger -nx-mr-2 nx-rounded nx-p-2 active:nx-bg-gray-400/20 md:nx-hidden"><svg fill="none" width="24" height="24" viewBox="0 0 24 24" stroke="currentColor" className={menuOpen && "open"}><g><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16"></path></g><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 12h16"></path><g><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 18h16"></path></g></svg></button>
-      </div>
+              </ul>
+            </div>
+            <div id="login-nav-button"><a href="http://radical.sh/dashboard">Sign In</a></div>
+            <button onClick={() => {
+              const asideElement = document.getElementsByTagName("aside")
+              //asideElement[0].className = "nextra-sidebar-container nx-flex nx-flex-col md:nx-top-16 md:nx-shrink-0 motion-reduce:nx-transform-none nx-transform-gpu nx-transition-all nx-ease-in-out md:nx-w-64 md:nx-sticky md:nx-self-start max-md:[transform:translate3d(0,-100%,0)]"
+              if (!menuOpen) {
+                asideElement[0].className = "nextra-sidebar-container nx-flex nx-flex-col md:nx-top-16 md:nx-shrink-0 motion-reduce:nx-transform-none nx-transform-gpu nx-transition-all nx-ease-in-out md:nx-w-64 md:nx-sticky md:nx-self-start max-md:[transform:translate3d(0,0,0)]"
+              }
+              else {
+                asideElement[0].className = "nextra-sidebar-container nx-flex nx-flex-col md:nx-top-16 md:nx-shrink-0 motion-reduce:nx-transform-none nx-transform-gpu nx-transition-all nx-ease-in-out md:nx-w-64 md:nx-sticky md:nx-self-start max-md:[transform:translate3d(0,-100%,0)]"
+              }
+              setIsMenuOpen(!menuOpen)
+            }} type="button" aria-label="Menu" className="nextra-hamburger -nx-mr-2 nx-rounded nx-p-2 active:nx-bg-gray-400/20 md:nx-hidden"><svg fill="none" width="24" height="24" viewBox="0 0 24 24" stroke="currentColor" className={menuOpen && "open"}><g><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16"></path></g><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 12h16"></path><g><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 18h16"></path></g></svg></button>
+          </div>
+        </>)
+
     }
   },
   feedback: {
